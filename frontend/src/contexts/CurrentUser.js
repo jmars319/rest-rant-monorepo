279d3bbe606de886143007bb0bce8b5b4eab6fef
Jason Marshall <jam319@protonmail.com>
2022-08-15 00:44:08 -0400

    completed assignment

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -8,13 +8,19 @@ function CurrentUserProvider({ children }) {
     useEffect(() => {
 
         const getLoggedInUser = async () => {
-            let response = await fetch('http://localhost:4000/authentication/profile')
+            let response = await fetch('http://localhost:4000/authentication/profile', {
+                credentials: 'include'
+            })
             let user = await response.json()
             setCurrentUser(user)
         }
         getLoggedInUser()
     }, [])
-  
+    return (
+        <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
+            {children}
+        </CurrentUser.Provider>
+    )
 }
 
 export default CurrentUserProvider
